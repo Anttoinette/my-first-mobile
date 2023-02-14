a5177d98046f6f8a1249203c031eaa45e00f255b
Anttoinette Henry <henryanttoinette@icloud.com>
2023-02-14 14:57:03 -0500

    my-first-mobile

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>You clicked {count} times.</Text>
+      <Button title="Click me!" onPress={() => setCount(count + 1)} />
       <StatusBar style="auto" />
     </View>
   );
